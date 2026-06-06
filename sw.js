850c0d818496cf9c5541d5db438eb994cd9f716c
Stella Kadiri <stellakadri@gmail.com>
2026-06-06 10:01:19 +0100

    Add PWA manifest, service worker and icons

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'lodgelink-v1';
+const ASSETS = [
+  '/index.html',
+  '/host.html',
+  '/admin.html',
+  '/success.html',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('/index.html');
+    })
+  );
+});
